858c92b616c0f725445b0df9b0e30a9a4c1fc4f4
jaimitin <jaimitin11@yahoo.com>
2022-12-31 19:39:56 -0500

    Fix string extension

--- Perseus.Core/StringExtensions/StringExtensions.cs
+++ Perseus.Core/StringExtensions/StringExtensions.cs
@@ -5,8 +5,8 @@
     /// </summary>
     public static class StringExtensions
     {
-        public static bool IsNullOrWhiteSpace(this string value) => !string.IsNullOrWhiteSpace(value);
+        public static bool IsNullOrWhiteSpace(this string? value) => !string.IsNullOrWhiteSpace(value);
 
-        public static bool IsNullOrEmpty(this string value) => !string.IsNullOrEmpty(value);
+        public static bool IsNullOrEmpty(this string? value) => !string.IsNullOrEmpty(value);
     }
 }
